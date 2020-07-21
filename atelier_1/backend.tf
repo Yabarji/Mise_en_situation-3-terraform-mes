eccebb3962d90e8512d10e096fc55c9cfe988947
Yassen ABARJI <yabarji1@gmail.com>
2020-07-21 16:04:33 +0200

    Correction files backend.tf config S3 (ligne bucket)

--- atelier_1/backend.tf
+++ atelier_1/backend.tf
@@ -1,11 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "0afc-4d0a-8062-stagiaire"
+    bucket = "0afc-4d0a-8062-stagiaire01"
     key    = "backends/kapsule/01/states"
     region = "fr-par"
     endpoint = "https://s3.fr-par.scw.cloud"
-
     skip_credentials_validation = true
     skip_region_validation = true
-}
+ }
 }
