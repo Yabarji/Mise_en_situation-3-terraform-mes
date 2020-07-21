f89590967970afe2557122a6553044711345f775
Yassen ABARJI <yabarji1@gmail.com>
2020-07-21 18:35:43 +0200

    Deploiement cluster Kubernetes (Kapsule) sur Scaleway (Noeuds type DEV1-M)

---
+++ atelier_1/main.tf
@@ -0,0 +1,13 @@
+### Conf Security Group ###cluster Kubernetes ###
+resource "scaleway_k8s_cluster_beta" "s01-yassen" {
+  name = "s01-yassen"
+  version = "1.18.0"
+  cni = "cilium"
+}
+
+resource "scaleway_k8s_pool_beta" "s01-yassen" {
+  cluster_id = scaleway_k8s_cluster_beta.jack.id
+  name = "s01-yassen"
+  node_type = "DEV1-M"
+  size = 3
+}
--- atelier_1/provider.tf
+++ atelier_1/provider.tf
@@ -1,1 +1,12 @@
-provider scaleway {}
+terraform {
+  required_providers {
+    scaleway = {
+      source = "terraform-providers/scaleway"
+      version = "~> 1.15"
+    }
+  }
+  required_version = "~> 0.13"
+}
+
+provider "scaleway" {
+}
