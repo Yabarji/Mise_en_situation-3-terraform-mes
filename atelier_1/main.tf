257959c2ba3c03907ae7b13160b7cc788e49556b
Yassen ABARJI <yabarji1@gmail.com>
2020-07-22 09:47:51 +0200

    Changement nom cluster en ingecloud-eks-s01

--- atelier_1/main.tf
+++ atelier_1/main.tf
@@ -1,13 +1,13 @@
 ### Conf Security Group ###cluster Kubernetes ###
-resource "scaleway_k8s_cluster_beta" "ingecloud-eks-01" {
-  name = "s01-yassen"
+resource "scaleway_k8s_cluster_beta" "ingecloud-eks-s01" {
+  name = "ingecloud-eks-s01"
   version = "1.18.6"
   cni = "cilium"
 }
 
-resource "scaleway_k8s_pool_beta" "ingecloud-eks-01" {
-  cluster_id = scaleway_k8s_cluster_beta.s01-yassen.id
-  name = "s01-yassen"
+resource "scaleway_k8s_pool_beta" "ingecloud-eks-s01" {
+  cluster_id = scaleway_k8s_cluster_beta.ingecloud-eks-s01.id
+  name = "ingecloud-eks-s01"
   node_type = "DEV1-M"
   size = 2
 }
