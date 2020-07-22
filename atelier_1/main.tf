7dda29481caa66156acd2f64e1e23678288e38b6
Yassen ABARJI <yabarji1@gmail.com>
2020-07-22 17:44:35 +0200

    intro erreur de syntaxe volontaire dans fichier main.tf

--- atelier_1/main.tf
+++ atelier_1/main.tf
@@ -6,7 +6,7 @@ resource "scaleway_k8s_cluster_beta" "ingecloud-eks-01" {
 }
 
 resource "scaleway_k8s_pool_beta" "ingecloud-eks-s01" {
-  cluster_id = scaleway_k8s_cluster_beta.ingecloud-eks-01.id
+  cluster_id = scaleway_k8s_cluster_beta.ingecloud-eks-s01.id
   name = "ingecloud-eks-01"
   node_type = "DEV1-M"
   size = 2
