7ca0a4d96d05e01dbe1766ac8cc6fee7ff93e011
Yassen ABARJI <yabarji1@gmail.com>
2020-07-22 15:21:02 +0200

    vérification recette avec tflint via la Gitlab CI (erreur de syntaxe pour controle avec correction - 2)

--- atelier_1/main.tf
+++ atelier_1/main.tf
@@ -1,13 +1,13 @@
 ### Conf Security Group ###cluster Kubernetes ###
-resource "scaleway_k8s_cluster_beta" "ingecloud-eks-s01" {
-  name = "ingecloud-eks-s01
+resource "scaleway_k8s_cluster_beta" "ingecloud-eks-01" {
+  name = "ingecloud-eks-01"
   version = "1.18.6"
   cni = "cilium"
 }
 
 resource "scaleway_k8s_pool_beta" "ingecloud-eks-s01" {
   cluster_id = scaleway_k8s_cluster_beta.ingecloud-eks-s01.id
-  name = "ingecloud-eks-s01"
+  name = "ingecloud-eks-01"
   node_type = "DEV1-M"
   size = 2
 }
