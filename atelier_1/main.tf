7f17afa4671bc0e63b691d053d3f56b5339cebc3
Yassen ABARJI <yabarji1@gmail.com>
2020-07-23 11:04:38 +0200

    Test Parallelisation Job

--- atelier_1/main.tf
+++ atelier_1/main.tf
@@ -6,8 +6,8 @@ resource "scaleway_k8s_cluster_beta" "ingecloud-eks-01" {
 }
 
 resource "scaleway_k8s_pool_beta" "ingecloud-eks-01" {
-  cluster_id = scaleway_k8s_cluster_beta.ingecloud-eks-sss01.id
+  cluster_id = scaleway_k8s_cluster_beta.ingecloud-eks-01.id
   name = "ingecloud-eks-01"
   node_type = "DEV1-M"
   size = 2
-}
+
