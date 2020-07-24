5be87a3ba69f07b281ad26fe51c6a987319a7dc6
Yassen ABARJI <yabarji1@gmail.com>
2020-07-24 15:54:38 +0200

    modif du fichier provider.tf et du main.tf

--- atelier_1/main.tf
+++ atelier_1/main.tf
@@ -1,15 +1,3 @@
-required_providers {
-    scaleway = {
-      source = "terraform-providers/scaleway"
-      version = "~> 1.15"
-    }
-  }
-  required_version = "~> 0.12"
-}
-
-provider "scaleway" {
-}
-
 ### Conf Security Group ###cluster Kubernetes ###
 resource "scaleway_k8s_cluster_beta" "ingecloud-eks-01" {
   name = "ingecloud-eks-01"
@@ -22,30 +10,5 @@ resource "scaleway_k8s_pool_beta" "ingecloud-eks-01" {
   name = "ingecloud-eks-01"
   node_type = "DEV1-M"
   size = 2
-}
-
-provider "helm" {
-  kubernetes {
-    host = scaleway_k8s_cluster_beta.foobar.kubeconfig[0].host
-    token = scaleway_k8s_cluster_beta.foobar.kubeconfig[0].token
-    cluster_ca_certificate =  base64decode(scaleway_k8s_cluster_beta.ingecloud-eks-05.kubeconfig[0].cluster_ca_certificate)
-    load_config_file = false
-  }
-}
-
- # maint.tf
- # service.tf
- resource "helm_release" "kubeapps" {
-  name       = "kubeapps"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "kubeapps"
-  version    = "3.7.4"
-  namespace  = "kubeapps"
-  create_namespace  = true
-
-  set {
-    name  = "useHelm3"
-    value = "true"
-  }
 }
 
---
+++ atelier_1/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    scaleway = {
+      source = "terraform-providers/scaleway"
+      version = "~> 1.15"
+    }
+  }
+  required_version = "~> 0.12"
+}
+
+provider "scaleway" {
+}
+
+provider "helm"{
+  kubernetes {
+    host = scaleway_k8s_cluster_beta.foobar.kubeconfig[0].host
+    token = scaleway_k8s_cluster_beta.foobar.kubeconfig[0].token
+    cluster_ca_certificate = scaleway_k8s_cluster_beta.foobar.kubeconfig[0].cluster_ca_certificate
+    host  = scaleway_k8s_cluster_beta.ingecloud-eks-01.kubeconfig[0].host
+    token = scaleway_k8s_cluster_beta.ingecloud-eks-01.kubeconfig[0].token
+    cluster_ca_certificate = base64decode(scaleway_k8s_cluster_beta.ingecloud-eks-01.kubeconfig[0].cluster_ca_certificate)
+    load_config_file = false
+  }
+}
