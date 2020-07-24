3c21d7265d31dc6b5c48e5ddb02ee836d7668b61
Yassen ABARJI <yabarji1@gmail.com>
2020-07-24 15:35:00 +0200

    modif du fichier provider.tf et du mian.tf

--- atelier_1/main.tf
+++ atelier_1/main.tf
@@ -11,3 +11,29 @@ resource "scaleway_k8s_pool_beta" "ingecloud-eks-01" {
   node_type = "DEV1-M"
   size = 2
 }
+
+provider "helm" {
+  kubernetes {
+    host = scaleway_k8s_cluster_beta.foobar.kubeconfig[0].host
+    token = scaleway_k8s_cluster_beta.foobar.kubeconfig[0].token
+    cluster_ca_certificate =  base64decode(scaleway_k8s_cluster_beta.ingecloud-eks-05.kubeconfig[0].cluster_ca_certificate)
+    load_config_file = false
+  }
+}
+
+ # maint.tf
+ # service.tf
+ resource "helm_release" "kubeapps" {
+  name       = "kubeapps"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "kubeapps"
+  version    = "3.7.4"
+  namespace  = "kubeapps"
+  create_namespace  = true
+
+  set {
+    name  = "useHelm3"
+    value = "true"
+  }
+}
+
--- atelier_1/provider.tf
+++ atelier_1/provider.tf
@@ -1,4 +1,3 @@
-terraform {
   required_providers {
     scaleway = {
       source = "terraform-providers/scaleway"
@@ -9,28 +8,5 @@ terraform {
 }
 
 provider "scaleway" {
-}
-
-provider "helm"{
-  kubernetes {
-    host  = scaleway_k8s_cluster_beta.ingecloud-eks-05.kubeconfig[0].host
-    token = scaleway_k8s_cluster_beta.ingecloud-eks-05.kubeconfig[0].token
-    cluster_ca_certificate = base64decode(scaleway_k8s_cluster_beta.ingecloud-eks-05.kubeconfig[0].cluster_ca_certificate)
-    load_config_file = false
-  }
-}
-
-resource "helm_release" "kubeapp"{
-  name       = "kubeapps"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "kubeapps"
-  version    = "3.7.4"
-  namespace  = "kubeapps"
-  create_namespace = true
-
-  set {
-  name  = "useHelm3"
-  value = "true"
-  }
 }
 
