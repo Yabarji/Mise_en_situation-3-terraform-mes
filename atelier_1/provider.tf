07e36804f8219c608cf7873cdea0d10a6e3edec3
Yassen ABARJI <yabarji1@gmail.com>
2020-07-24 15:15:15 +0200

    modif du fichier provider.tf

--- atelier_1/provider.tf
+++ atelier_1/provider.tf
@@ -11,12 +11,39 @@ terraform {
 provider "scaleway" {
 }
 
-provider "helm" {
+resource "scaleway_k8s_cluster_beta" "ingecloud-eks-05" {
+  name    = "ingecloud-eks-05"
+  version = "1.18.0"
+  cni     = "cilium"
+}
+
+resource "scaleway_k8s_pool_beta" "ingecloud-eks-05" {
+  cluster_id = scaleway_k8s_cluster_beta.ingecloud-eks-05.id
+  name       = "ingeclouds-eks-05"
+  node_type  = "DEV1-M"
+  size       = 2
+}
+
+provider "helm"{
   kubernetes {
-    host = scaleway_k8s_cluster_beta.foobar.kubeconfig[0].host
-    token = scaleway_k8s_cluster_beta.foobar.kubeconfig[0].token
-    cluster_ca_certificate = scaleway_k8s_cluster_beta.foobar.kubeconfig[0].cluster_ca_certificate
+    host  = scaleway_k8s_cluster_beta.ingecloud-eks-05.kubeconfig[0].host
+    token = scaleway_k8s_cluster_beta.ingecloud-eks-05.kubeconfig[0].token
+    cluster_ca_certificate = scaleway_k8s_cluster_beta.ingecloud-eks-05.kubeconfig[0].cluster_ca_certificate
     load_config_file = false
+  }
+}
+
+resource "helm_release" "kubeapp"{
+  name       = "kubeapps"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "kubeapps"
+  version    = "3.7.4"
+  namespace  = "kubeapps"
+  create_namespace = true
+
+  set {
+  name  = "useHelm3"
+  value = "true"
   }
 }
 
